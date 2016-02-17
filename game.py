e7a4c6bf5b3db39a2e70a8890e15636c3facfbaf
Bruno Nova <brunomb.nova@gmail.com>
2016-02-17 16:49:55 +0000

    Customize main menu and add options menu

--- game.py
+++ game.py
@@ -7,13 +7,19 @@ from cocos.scene import Scene
 from cocos.text import Label
 from gettext import gettext as _
 
+import constants
 from balls import *
 
 
 class GameScene(Scene):
 	"""The scene that runs the actual game."""
-	def __init__(self):
-		gameLayer = GameLayer()
+	def __init__(self, options):
+		"""
+		Creates the scene.
+
+		@param options: game options (a dictionary).
+		"""
+		gameLayer = GameLayer(options)
 		hudLayer = HUDLayer(gameLayer)
 		super().__init__(gameLayer, hudLayer)
 
@@ -26,11 +32,13 @@ class HUDLayer(Layer):
 		self.gameLayer = gameLayer
 		self.time = 0
 
-		self.lblTime = Label(bold=True, color=(0, 0, 0, 255), anchor_x="left", anchor_y="bottom")
+		self.lblTime = Label(bold=True, color=constants.FONT_COLOR,
+		                     anchor_x="left", anchor_y="bottom")
 		self.lblTime.position = 10, 0
 		self.add(self.lblTime)
 
-		self.lblEnemies = Label(bold=True, color=(0, 0, 0, 255), anchor_x="right", anchor_y="bottom")
+		self.lblEnemies = Label(bold=True, color=constants.FONT_COLOR,
+		                        anchor_x="right", anchor_y="bottom")
 		self.lblEnemies.position = director.window.width - 10, 0
 		self.add(self.lblEnemies)
 
@@ -47,8 +55,14 @@ class GameLayer(ColorLayer):
 	"""Layer that shows and controls the actual game."""
 	is_event_handler = True
 
-	def __init__(self):
-		super().__init__(192, 192, 192, 255)
+	def __init__(self, options):
+		"""
+		Creates the layer.
+
+		@param options: game options (a dictionary).
+		"""
+		super().__init__(*constants.BACKGROUND_COLOR)
+		self.options = options
 		self.keysPressed = set()
 		self.mouseDelta = Vector2(0, 0)
 		self.collMan = CollisionManagerGrid(0, 0, self.width, self.height, 30, 30)
@@ -124,11 +138,12 @@ class GameLayer(ColorLayer):
 					self.gameOver()
 
 			# Check collisions between enemies
-			for i, enemy in enumerate(self.enemies):
-				for j, other in enumerate(self.enemies):
-					if j > i and enemy.enabled and other.enabled \
-					   and self.collMan.they_collide(enemy, other):
-						Enemy.bounceBalls(enemy, other)
+			if self.options["ballsCollide"]:
+				for i, enemy in enumerate(self.enemies):
+					for j, other in enumerate(self.enemies):
+						if j > i and enemy.enabled and other.enabled \
+						   and self.collMan.they_collide(enemy, other):
+							Enemy.bounceBalls(enemy, other)
 
 	def on_key_press(self, key, modifiers):
 		self.keysPressed.add(key)
--- menu.py
+++ menu.py
@@ -1,30 +1,49 @@
 import pyglet
 from cocos.director import director
-from cocos.menu import Menu, MenuItem
+from cocos.layer import ColorLayer, MultiplexLayer
+from cocos.menu import Menu, MenuItem, ToggleMenuItem
 from cocos.scene import Scene
 from gettext import gettext as _
 
+import constants
 from game import GameScene
 
 
 class MenuScene(Scene):
 	"""The scene that displays the main menu."""
 	def __init__(self):
-		super().__init__(MenuLayer())
+		menuLayer = MenuLayer()
+		optionsLayer = OptionsLayer(menuLayer)
+		super().__init__(ColorLayer(*constants.BACKGROUND_COLOR),
+						 MultiplexLayer(menuLayer, optionsLayer))
 
 
 class MenuLayer(Menu):
 	"""Layer that displays the menu."""
 	def __init__(self):
 		super().__init__("Collision")
-		self.menu_anchor_x = self.menu_anchor_y = "center"
 
-		items = [MenuItem(_("Play"), self.onPlay),
-		         MenuItem(_("Quit"), self.on_quit)]
+		# Initialize game options
+		self.options = {"ballsCollide": True}
+
+		# Customize the menu
+		self.font_title["color"] = constants.FONT_COLOR
+		self.font_item["color"] = constants.FONT_COLOR_NOT_SELECTED
+		self.font_item_selected["color"] = constants.FONT_COLOR
+
+		# Add the items and create the menu
+		items = [
+			MenuItem(_("Play"), self.onPlay),
+			MenuItem(_("Options"), self.onOptions),
+			MenuItem(_("Quit"), self.on_quit)
+		]
 		self.create_menu(items)
 
 	def onPlay(self):
-		director.push(GameScene())
+		director.push(GameScene(self.options))
+
+	def onOptions(self):
+		self.parent.switch_to(1)
 
 	def on_enter(self):
 		super().on_enter()
@@ -32,3 +51,30 @@ class MenuLayer(Menu):
 
 	def on_quit(self):
 		pyglet.app.exit()
+
+
+class OptionsLayer(Menu):
+	"""Layer that display an options menu."""
+
+	def __init__(self, menuLayer):
+		super().__init__(_("Options"))
+		self.menuLayer = menuLayer
+
+		# Customize the menu
+		self.font_title["color"] = constants.FONT_COLOR
+		self.font_item["color"] = constants.FONT_COLOR_NOT_SELECTED
+		self.font_item_selected["color"] = constants.FONT_COLOR
+
+		# Add the items and create the menu
+		items = [
+			ToggleMenuItem(_("Ball collisions: "), self.onBallsCollide,
+			               self.menuLayer.options["ballsCollide"]),
+			MenuItem(_("Quit"), self.on_quit)
+		]
+		self.create_menu(items)
+
+	def onBallsCollide(self, value):
+		self.menuLayer.options["ballsCollide"] = value
+
+	def on_quit(self):
+		self.parent.switch_to(0)
