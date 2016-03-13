fc3154bd4a89d316d03c93279e07e65f1cfa0cc5
Bruno Nova <brunomb.nova@gmail.com>
2016-03-13 12:13:37 +0000

    Add INSTALL.md, fill README.md, improve setup.py

---
+++ INSTALL.md
@@ -0,0 +1,14 @@
+Installation instruction
+========================
+
+1.  Make sure that python3, along with pip, is installed.
+
+2.  Open a terminal in the project directory and run:
+
+        sudo pip3 install .
+    
+    Or, if you want to install the game to the home folder as a user, run:
+    
+        pip3 install --user .
+    
+    If the `pip3` command is not available in your system, use `pip` instead.
--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
+import os
 from setuptools import setup, find_packages
+
+# Find directory path
+path = os.path.dirname(os.path.abspath(__file__))
+
+# Read README.md file
+with open(os.path.join(path, "README.md")) as f:
+	longDescription = f.read()
 
 setup(
 	name = "Collision",
 	version = "0.0.1",
 	author = "Bruno Nova",
 	author_email = "brunomb.nova@gmail.com",
-	description = "A game where you control a ball while dodging the others for as long as possible",
-	#license = "?",
+	description = "A ball dodging game",
+	long_description = longDescription,
+	license = "GPLv3",
 	keywords = "game",
-	#url = "?",
+	url = "https://github.com/brunonova/collision",
 
 	packages = find_packages(),
 	package_data = {"collision": ["res/*", "mo/*/*/*.mo"]},
 	scripts = ["bin/collision"],
-	install_requires = ["cocos2d"]
+	install_requires = ["cocos2d"],
 )
